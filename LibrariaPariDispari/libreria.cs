0d2a3c5d8de9735f98c59168a7f069e86bdce66e
andreadgobbo@gmail.com <andreadgobbo@gmail.com>
2020-12-06 22:55:23 +0100

    Aggiungere i file di progetto.

---
+++ LibrariaPariDispari/libreria.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LibrariaPariDispari
+{
+    public class libreria
+    {
+        public class paridispariinterval
+        {
+            public static uint Leggiintero()
+            {
+                Console.Write("inserisci un numero intero e positivo: ");
+                uint a = uint.Parse(Console.ReadLine());
+                return a;
+            }
+            public static void PariDispari(uint a, uint b)
+            {
+                int contatoreP = 0, contatoreD = 0;
+                uint max = Math.Max(a, b);
+                uint min = Math.Min(a, b);
+                for (uint i = min; i <= max; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine($"{i}pari;");
+                        contatoreP++;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{i}dipari;");
+                        contatoreD++;
+                    }
+                }
+                Console.WriteLine($"i numeri sono pari{contatoreP}dispari{contatoreD}");
+            }
+        }
+    }
+}
---
+++ paridispari/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LibrariaPariDispari;
+
+namespace paridispari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint n = libreria.Leggiintero();
+            uint n1 = libreria.leggiintero();
+            paridispari.PariDispari(n, n1);
+
+        }
+    }
+}
